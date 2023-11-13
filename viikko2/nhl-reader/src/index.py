832e260b38bea1108b9b3bb077fd9f3cfe534aa6
Anna Essina <101710774+annaessina@users.noreply.github.com>
2023-11-13 22:06:02 +0200

    Update index.py

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -7,7 +7,7 @@ def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
     reader = PlayerReader(url)
     stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("NYI")
+    players = stats.top_scorers_by_nationality("FIN")
 
     for player in players:
         print(player)
