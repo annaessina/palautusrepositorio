8f316c9f4194bfec7fd804afcb9aed0d20500e02
Anna Essina <anna.essina@helsinki.fi>
2023-12-04 22:27:18 +0200

    tennis tehtävä

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,43 +1,51 @@
+
 class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1 = player1_name
         self.player2 = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
-        self.points = ["Love", "Fifteen", "Thirty", "Forty"]
+        self.score1 = 0
+        self.score2 = 0
+        self.scoret = ["Love", "Fifteen", "Thirty", "Forty"]
 
     def won_point(self, player_name):
         if player_name == "player1":
-            self.m_score1 += 1
+            self.score1 += 1
         else:
-            self.m_score2 += 1
+            self.score2 += 1
 
     def get_score(self):
-        if self.m_score1 == self.m_score2:
-            return self.draw_game()
-        if self.m_score1 >= 4 or self.m_score2 >= 4:
-            return self.current_result()
-        else:
-            return f"{self.points[self.m_score1]}-{self.points[self.m_score2]}"
+        if self.score1 == self.score2:
+            return self.tasapeli()
+        if self.score1 >= 4 or self.score2 >= 4:
+            return self.johto_tai_voitto()
+        return self.peli_jatkuu()
 
 
-    def draw_game(self):
-        if self.m_score1 < 4:
-            score = f"{self.points[self.m_score1]}-All"
+    def tasapeli(self):
+        if self.score1 < 4:
+            tulos = self.scoret[self.score1]
+            score = f"{tulos}-All"
         else:
             score = "Deuce"
 
         return score
-    def current_result(self):
-        difference = self.m_score1 - self.m_score2
+    def johto_tai_voitto(self):
+        minus_result = self.score1 - self. score2
 
-        if difference == 1:
+        if minus_result == 1:
             score = "Advantage player1"
-        elif difference == -1:
+        elif minus_result == -1:
             score = "Advantage player2"
-        elif difference >= 2:
+        elif minus_result >= 2:
             score = "Win for player1"
         else:
             score = "Win for player2"
+
+        return score
+    def peli_jatkuu(self):
+        score1 = self.scoret[self.score1]
+        score2 = self.scoret[self.score2]
+
+        score = f"{score1}-{score2}"
 
         return score
